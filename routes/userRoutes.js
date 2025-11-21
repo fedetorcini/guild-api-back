974df5231cf4eb0e8cce5755ad49a580bd429fbe
Felipe Alzamora <luisfalmi@gmail.com>
2025-11-21 18:54:49 -0300

    add list followers and following

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -12,5 +12,17 @@ router.put('/me', authenticate, userController.updateCurrentUser);
 // GET /users/:id - Get user by ID (requires auth)
 router.get('/:id', authenticate, userController.getUserById);
 
+// POST /users/:id/follow - Follow an user by id(requires auth)
+router.post('/:id/follow', authenticate, userController.followUser);
+
+// POST /users/:id/unfollow - Unfollow an user by id(requires auth)
+router.post('/:id/unfollow', authenticate, userController.unfollowUser);
+
+// GET /users/:id/followers - Get a list users followers(requires auth)
+router.get('/:id/followers', authenticate, userController.getFollowers);
+
+// GET /users/:id/following - Get a list users following(requires auth)
+router.get('/:id/following', authenticate, userController.getFollowing);
+
 module.exports = router;
 
